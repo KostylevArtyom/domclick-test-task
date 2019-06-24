f4f7dc26a2f04e81eee91f22b526b2195f524c54
Kostylev Artyom <KostylevArtjom@gmail.com>
2019-06-24 23:11:50 +0300

    Tests fixed

--- src/test/java/ru/domclick/testTask/service/AccountServiceTest.java
+++ src/test/java/ru/domclick/testTask/service/AccountServiceTest.java
@@ -20,123 +20,171 @@ public class AccountServiceTest {
     @MockBean
     private AccountRepository repository;
 
+    @MockBean
+    private ValidationService validator;
+
     @Autowired
     AccountService service;
 
     @Test
     public void get_ExistingId_Success() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ZERO)));
-        Account account = service.get(1L);
+        final Long id = 1L;
+        Mockito.when(repository.findById(id))
+            .thenReturn(Optional.of(new Account(id, BigDecimal.ZERO)));
+        Account account = service.get(id);
         Assert.assertEquals(account.getAmount(), BigDecimal.ZERO);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void get_NonExistingId_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
+        final Long id = 1L;
+        Mockito.when(repository.findById(id))
             .thenReturn(Optional.empty());
-        service.get(1L);
+        service.get(id);
     }
 
     @Test
     public void replenish_ExistingIdAndPositiveAmount_Success() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ZERO)));
-        service.replenish(1L, BigDecimal.ONE);
-        Account account = service.get(1L);
+        final Long id = 1L;
+        Mockito.when(repository.findById(id))
+            .thenReturn(Optional.of(new Account(id, BigDecimal.ZERO)));
+        service.replenish(id, BigDecimal.ONE);
+        Account account = service.get(id);
         Assert.assertEquals(account.getAmount(), BigDecimal.ONE);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void replenish_NonExistingIdAndPositiveAmount_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
+        final Long id = 1L;
+        Mockito.when(repository.findById(id))
             .thenReturn(Optional.empty());
-        service.replenish(1L, BigDecimal.ONE);
+        service.replenish(id, BigDecimal.ONE);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void replenish_ExistingIdAndZeroAmount_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ZERO)));
-        service.replenish(1L, BigDecimal.ZERO);
+        final Long id = 1L;
+        Mockito.doThrow(new IllegalArgumentException())
+            .when(validator)
+            .validateAmountIsPositive(BigDecimal.ZERO);
+        Mockito.when(repository.findById(id))
+            .thenReturn(Optional.of(new Account(id, BigDecimal.ZERO)));
+        service.replenish(id, BigDecimal.ZERO);
     }
 
     @Test
     public void withdraw_ExistingIdAndPositiveAmount_Success() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ONE)));
-        service.withdraw(1L, BigDecimal.ONE);
-        Account account = service.get(1L);
+        final Long id = 1L;
+        Mockito.when(repository.findById(id))
+            .thenReturn(Optional.of(new Account(id, BigDecimal.ONE)));
+        service.withdraw(id, BigDecimal.ONE);
+        Account account = service.get(id);
         Assert.assertEquals(account.getAmount(), BigDecimal.ZERO);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void withdraw_NonExistingIdAndPositiveAmount_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
+        final Long id = 1L;
+        Mockito.when(repository.findById(id))
             .thenReturn(Optional.empty());
-        service.withdraw(1L, BigDecimal.ONE);
+        service.withdraw(id, BigDecimal.ONE);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void withdraw_ExistingIdAndZeroAmount_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ONE)));
-        service.withdraw(1L, BigDecimal.ZERO);
+        final Long id = 1L;
+        Mockito.doThrow(new IllegalArgumentException())
+            .when(validator)
+            .validateAmountIsPositive(BigDecimal.ZERO);
+        Mockito.when(repository.findById(id))
+            .thenReturn(Optional.of(new Account(id, BigDecimal.ONE)));
+        service.withdraw(id, BigDecimal.ZERO);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void withdraw_ExistingIdAndExceededAmount_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ZERO)));
-        service.withdraw(1L, BigDecimal.ONE);
+        final Long id = 1L;
+        Mockito.doThrow(new IllegalArgumentException())
+            .when(validator)
+            .validateAmountIsNonNegative(BigDecimal.ONE.negate());
+        Mockito.when(repository.findById(id))
+            .thenReturn(Optional.of(new Account(id, BigDecimal.ZERO)));
+        service.withdraw(id, BigDecimal.ONE);
     }
 
     @Test
     public void transfer_ExistingIdsAndPositiveAmount_Success() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ONE)));
-        Mockito.when(repository.findById(2L))
-            .thenReturn(Optional.of(new Account(2L, BigDecimal.ONE)));
-        service.transfer(1L, 2L, BigDecimal.ONE);
-        Account first = service.get(1L);
-        Account second = service.get(2L);
+        final Long sender = 1L;
+        final Long recipient = 2L;
+        Mockito.when(repository.findById(sender))
+            .thenReturn(Optional.of(new Account(sender, BigDecimal.ONE)));
+        Mockito.when(repository.findById(recipient))
+            .thenReturn(Optional.of(new Account(recipient, BigDecimal.ONE)));
+        service.transfer(sender, recipient, BigDecimal.ONE);
+        Account first = service.get(sender);
+        Account second = service.get(recipient);
         Assert.assertEquals(first.getAmount(), BigDecimal.ZERO);
         Assert.assertEquals(second.getAmount(), BigDecimal.valueOf(2));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void transfer_NonExistingSender_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
+        final Long sender = 1L;
+        final Long recipient = 2L;
+        Mockito.when(repository.findById(sender))
             .thenReturn(Optional.empty());
-        Mockito.when(repository.findById(2L))
-            .thenReturn(Optional.of(new Account(2L, BigDecimal.ONE)));
-        service.transfer(1L, 2L, BigDecimal.ONE);
+        Mockito.when(repository.findById(recipient))
+            .thenReturn(Optional.of(new Account(recipient, BigDecimal.ONE)));
+        service.transfer(sender, recipient, BigDecimal.ONE);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void transfer_NonExistingRecipient_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ONE)));
-        Mockito.when(repository.findById(2L))
+        final Long sender = 1L;
+        final Long recipient = 2L;
+        Mockito.when(repository.findById(sender))
+            .thenReturn(Optional.of(new Account(sender, BigDecimal.ONE)));
+        Mockito.when(repository.findById(recipient))
             .thenReturn(Optional.empty());
-        service.transfer(1L, 2L, BigDecimal.ONE);
+        service.transfer(sender, recipient, BigDecimal.ONE);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void transfer_EqualSenderAndRecipient_ExceptionThrown() {
+        final Long sender = 1L;
+        Mockito.doThrow(new IllegalArgumentException())
+            .when(validator)
+            .validateSenderAndRecipientAreDifferent(sender, sender);
+        Mockito.when(repository.findById(sender))
+            .thenReturn(Optional.of(new Account(sender, BigDecimal.ONE)));
+        service.transfer(sender, sender, BigDecimal.ONE);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void transfer_NonPositiveAmount_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ONE)));
-        Mockito.when(repository.findById(2L))
-            .thenReturn(Optional.of(new Account(2L, BigDecimal.ONE)));
-        service.transfer(1L, 2L, BigDecimal.ZERO);
+        final Long sender = 1L;
+        final Long recipient = 2L;
+        Mockito.doThrow(new IllegalArgumentException())
+            .when(validator)
+            .validateAmountIsPositive(BigDecimal.ZERO);
+        Mockito.when(repository.findById(sender))
+            .thenReturn(Optional.of(new Account(sender, BigDecimal.ONE)));
+        Mockito.when(repository.findById(recipient))
+            .thenReturn(Optional.of(new Account(recipient, BigDecimal.ONE)));
+        service.transfer(sender, recipient, BigDecimal.ZERO);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void transfer_ExceededAmount_ExceptionThrown() {
-        Mockito.when(repository.findById(1L))
-            .thenReturn(Optional.of(new Account(1L, BigDecimal.ZERO)));
-        Mockito.when(repository.findById(2L))
-            .thenReturn(Optional.of(new Account(2L, BigDecimal.ZERO)));
-        service.transfer(1L, 2L, BigDecimal.ONE);
+        final Long sender = 1L;
+        final Long recipient = 2L;
+        Mockito.doThrow(new IllegalArgumentException())
+            .when(validator)
+            .validateAmountIsNonNegative(BigDecimal.ONE.negate());
+        Mockito.when(repository.findById(sender))
+            .thenReturn(Optional.of(new Account(sender, BigDecimal.ZERO)));
+        Mockito.when(repository.findById(recipient))
+            .thenReturn(Optional.of(new Account(recipient, BigDecimal.ZERO)));
+        service.transfer(sender, recipient, BigDecimal.ONE);
     }
 }
